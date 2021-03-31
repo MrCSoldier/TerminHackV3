f2662a914868199a92d8424a826c3e1401f8f80c
MrCSoldier <46113492+MrCSoldier@users.noreply.github.com>
2021-03-31 11:24:03 +0100

    UwU

---
+++ TerminHackV3/HardwareHandlers/CPUsage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TerminHackV3.HardwareHandlers
+{
+    class CPUsage : Hardware.IHardwareHandler
+    {
+        public float CPUusage(float slowdownProcessingSpeed) {
+            return slowdownProcessingSpeed;
+        }
+
+        public float DelayCommand(float slowdownProcessingSpeed) {
+            return slowdownProcessingSpeed;
+        }
+        /*if (CPusage >=  100) {
+            
+        }*/
+    }
+}
---
+++ TerminHackV3/HardwareHandlers/RAMUsage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TerminHackV3.HardwareHandlers
+{
+    class RAMUsage
+    {
+    }
+}
